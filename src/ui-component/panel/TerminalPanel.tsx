05ebbb5ed403695d8b118c7cf152b6fb385dbf44
kazumatu981 <kazumatu981@gmail.com>
2025-02-23 10:57:27 +0900

    Refactor TerminalPanel to handle command processing state and update welcome message

--- src/ui-component/panel/TerminalPanel.tsx
+++ src/ui-component/panel/TerminalPanel.tsx
@@ -1,34 +1,40 @@
 import { Terminal } from 'primereact/terminal';
 import { TerminalService } from 'primereact/terminalservice';
-import { useEffect, JSX, useContext } from 'react';
+import { useEffect, JSX, useContext, useState } from 'react';
 import { ExpressionContext } from '../ExpressionContext';
 
 import { resolveAsync } from '../../lib/resolver';
 import './TerminalPanel.scss';
 
+const welcomeMessage = '対話式計算機: 四則演算の数式を入力して"Enter"キーを押してください。';
 export function TerminalPanel(): JSX.Element {
+    const [isProcessing, setIsProcessing] = useState(false);
     const context = useContext(ExpressionContext);
     useEffect(() => {
-        TerminalService.on('command', (command: string) => {
-            context?.setExpression(command);
-            resolveAsync(command)
-                .then((result) => {
-                    TerminalService.emit('response', result);
-                })
-                .catch((error) => {
-                    TerminalService.emit('response', error.message);
-                });
-        });
+        TerminalService.on('command', onCommand);
         return () => {
             TerminalService.off('command', () => {});
         };
     }, [context]);
+
+    function onCommand(expression: string) {
+        setIsProcessing(true);
+        context?.setExpression(expression);
+        resolveAsync(expression)
+            .then((result) => {
+                TerminalService.emit('response', result);
+            })
+            .catch((error) => {
+                TerminalService.emit('response', error.message);
+            })
+            .finally(() => {
+                setIsProcessing(false);
+            });
+    }
+
     return (
         <div className="m-4">
-            <Terminal
-                welcomeMessage='Welcome to the Calculator.Please enter an "expression" and press Enter'
-                prompt="expression>"
-            />
+            <Terminal welcomeMessage={welcomeMessage} prompt="計算式> "></Terminal>
         </div>
     );
 }
